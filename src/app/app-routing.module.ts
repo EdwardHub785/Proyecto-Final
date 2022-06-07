2a3ce6a87ed5ae78ea69359734961ddce2d56bc5
Edward Laiton <edwlz@hotmail.com>
2022-06-07 16:33:03 -0500

    Mejora 2.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioCompComponent } from './component/inicio-comp/inicio-comp.component';
 import { LoginCompComponent } from './component/login-comp/login-comp.component';
+import { ReservaCompComponent } from './component/reserva-comp/reserva-comp.component';
 
 
 const routes: Routes = [
-  {path : 'inicio', component : InicioCompComponent},
-  {path: 'login', component: LoginCompComponent}
+  {path: 'inicio', component : InicioCompComponent},
+  {path: 'login', component: LoginCompComponent},
+  {path: 'reserva', component: ReservaCompComponent}
 ];
 
 @NgModule({
